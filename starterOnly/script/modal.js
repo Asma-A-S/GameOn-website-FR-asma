96dd7260ac0411e4c7d97174bcf8ebd48d034965
asma <asma.benzerrouk@gmail.com>
2024-02-27 15:38:04 +0100

    ajout condition afficher/cacher le background

--- starterOnly/script/modal.js
+++ starterOnly/script/modal.js
@@ -8,21 +8,34 @@ function editNav() {
 }
 
 // DOM Elements
+const footer = document.querySelector("footer")
+const nav = document.querySelector(".topnav")
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modal = document.querySelector(".close");
+const confirmModalClose = document.querySelector(".modal-confirmation-close")
+const background = document.querySelector(".hero-section")
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  background.style.display = "none";
+  footer.style.display = "none"
+  if (window.innerWidth < 768) {
+    nav.style.display = "block"; // Afficher la barre de navigation en version mobile
+  } else {
+    nav.style.display = "none"; // Cacher la barre de navigation en version bureau
+  }
 }
 // fermer la modale
 function closeModal() {
   modalbg.style.display = "none";
+  window.location.reload()
 }
 
 modal.addEventListener("click", closeModal)
+confirmModalClose.addEventListener("click", closeModal);
 
